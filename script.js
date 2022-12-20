3e405f5d52ee0c403e79be1362585ac5f47dc6a6
ncar7870 <121036356+ncar7870@users.noreply.github.com>
2022-12-20 08:53:29 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+var photos = [
+    "https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Grey%27s_Anatomy_Logo.svg/480px-Grey%27s_Anatomy_Logo.svg.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/The_Office_US_logo.svg/1280px-The_Office_US_logo.svg.png",
+    "https://live.staticflickr.com/2936/13473480295_6946a28e9d_c.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/e/ec/American_Horror_Story.svg",
+	"https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Dan_Vs_logo.svg/1200px-Dan_Vs_logo.svg.png"
+];
+
+$("body").append("<img src=" + photos[0] + ">");
+$("body").append("<img src=" + photos[1] + ">");
+$("body").append("<img src=" + photos[2] + ">");
+$("body").append("<img src=" + photos[3] + ">");
+$("body").append("<img src=" + photos[4] + ">");
+$("body").append("<img src=" + photos[5] + ">");
